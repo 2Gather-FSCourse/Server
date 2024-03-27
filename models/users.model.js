81209e474e0b40ab3f3943cd61efb06637e6ffb3
MoranSin <nurixhbh@gmail.com>
2024-03-27 13:45:20 +0200

    wee

--- models/users.model.js
+++ models/users.model.js
@@ -3,8 +3,9 @@ const { Schema, model } = require('mongoose');
 const usersSchema = new Schema({
     userType: { type: String, required: true },
     name: { type: String },
-    age: { type: Number },
+    age: { type: Number, required: true },
     img: { type: String },
+    phone: { type: Number, required: true },
     contribution: { type: Number, required: true },
     savedCards: { type: Array, required: true },
 }, { collection: 'users' });
