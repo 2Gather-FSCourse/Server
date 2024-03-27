d4d081ac5f04e81e1e0e981f7e43e8b5028c0520
Adi Levi <adile0194@gmail.com>
2024-03-27 12:50:08 +0200

    mongo storage functions

---
+++ database/mongoStorage.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose');
+const Path = require('path');
+const consts = require('../constants');
+
+const { DB_HOST, DB_USER, DB_PASS } = consts;
+
+class MongoStorage {
+    constructor(entity) {
+        this.entityName = entity.charAt(0).toLowerCase() + entity.slice(1);
+        this.Model = require(Path.join(__dirname, `../models/${this.entityName}.model.js`));
+        this.connect();
+    }
+
+    // eslint-disable-next-line class-methods-use-this
+    connect() {
+        const url = `mongodb+srv://${DB_USER}:${DB_PASS}@${DB_HOST}`;
+        mongoose
+            .connect(url)
+            .then(() => console.log('connected to MongoDB'))
+            .catch((err) => console.log(`connection error: ${err}`));
+    }
+
+    find() {
+        return this.Model.find({});
+    }
+
+    retrieve(id) {
+        return this.Model.findOne(id);
+    }
+
+    create(data) {
+        return this.Model.create(data);
+    }
+
+    delete(id) {
+        return this.Model.findByIdAndDelete(id);
+    }
+
+    update(id, data) {
+        return this.Model.findOneAndUpdate(id, data, { new: true });
+    }
+}
+
+module.exports = { MongoStorage };
