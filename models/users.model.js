2450e8170260e4bce7d6c3aebbce94997da65fb1
MoranSin <nurixhbh@gmail.com>
2024-03-27 22:18:23 +0200

    users model

--- models/users.model.js
+++ models/users.model.js
@@ -1,13 +1,13 @@
 const { Schema, model } = require('mongoose');
+const { donationsSchema } = require('./donations.model');
 
 const usersSchema = new Schema({
-    userType: { type: String, required: true },
+    userType: { type: String , enum: ["Donor","Organization"], required: true },
     name: { type: String },
     age: { type: Number, required: true },
     img: { type: String },
-    phone: { type: Number, required: true },
-    contribution: { type: Number, required: true },
-    savedCards: { type: Array, required: true },
+    phone: { type: String, required: true },
+    contribution: [donationsSchema],
 }, { collection: 'users' });
 
 module.exports = model('user', usersSchema);
