e8e0599df9c4083359e29a07ad4ec18fdefdacd9
MoranSin <nurixhbh@gmail.com>
2024-04-09 19:26:24 +0300

    cors for the boys?

--- app/app.js
+++ app/app.js
@@ -14,7 +14,7 @@ connect();
 const app = express();
 
 const corsOptions = {
-    origin: 'http://localhost:5173',
+    origin: 'http://localhost:5173','https://public-information-marag.netlify.app/': true,
     methods: 'GET,POST,PUT,DELETE',
     credentials: true,
 };
--- tests/donations.test.js
+++ tests/donations.test.js
@@ -4,3 +4,32 @@ const donationsRepository = require('../repositories/donationsRepository');
 const { ServerError } = require('../errors/errors');
 
 jest.mock('../repositories/donationsRepository');
+
+describe('GET /donations', () => {
+    beforeEach(()=> jest.clearAllMocks());
+
+    it('should return 200 and all donations', async () => {
+        const mockDonations = [{
+            _id:"6605aa2fb1c97be899f8526b",
+            campaignId:"660447a87061566700714e3a",
+            userId:"6605781abca1af49c582a734",
+            transactionId:"pi_3OzMqXDgt4I0wPiK18L5fkow",
+            amount:100,
+            confirmation:true,
+            itemList:[]
+        },{
+            _id:"6615433ca2089270235760a5",
+            campaignId:"661545026d0c1f83daad54df",
+            userId:"6613f10831cf830e508cf0e9",
+            transactionId:"pi_3OzMqXDgt4I0wPiK18L5fkow",
+            amount:100,
+            confirmation:true,
+            itemList:[],
+            transactionDate:"09/04/2024",
+        }];
+        donationsRepository.findDonations.mockResolvedValue(mockDonations);
+        const response = await request(app).get('/donations');
+        expect(response.status).toBe(200);
+        expect(response.body).toEqual(mockDonations);
+    });
+}
